abf794eb415164ea9406b744298ff0570a8e13d6
Paulo Filho <pcf.feat@gmail.com>
2022-10-19 14:14:10 -0400

    study: add annotations to the map with MKAnnotation-compliant delegate

---
+++ CapitalCities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  CapitalCities
+//
+//  Created by Paulo Filho on 19/10/22.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
--- CapitalCities/ViewController.swift
+++ CapitalCities/ViewController.swift
@@ -5,13 +5,23 @@
 //  Created by Paulo Filho on 19/10/22.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2022 Summer Olympics")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called City of Light")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
 
 
